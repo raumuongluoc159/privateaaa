a0a64ac02e0a8fa588c310619aebd05e90f3604c
raumuongluoc159 <59567448+raumuongluoc159@users.noreply.github.com>
2023-09-05 01:14:48 +0700

    predictor.py

--- roop/predictor.py
+++ roop/predictor.py
@@ -1,3 +1,4 @@
+
 import threading
 import numpy
 import opennsfw2
@@ -12,32 +13,16 @@ MAX_PROBABILITY = 0.85
 
 
 def get_predictor() -> Model:
-    global PREDICTOR
-
-    with THREAD_LOCK:
-        if PREDICTOR is None:
-            PREDICTOR = opennsfw2.make_open_nsfw_model()
-    return PREDICTOR
-
+    return None  # Thay vì trả về mô hình, hàm này sẽ trả về None
 
 def clear_predictor() -> None:
-    global PREDICTOR
-
-    PREDICTOR = None
-
+    pass  # Bỏ trống hàm này hoặc bạn có thể xóa nó đi
 
 def predict_frame(target_frame: Frame) -> bool:
-    image = Image.fromarray(target_frame)
-    image = opennsfw2.preprocess_image(image, opennsfw2.Preprocessing.YAHOO)
-    views = numpy.expand_dims(image, axis=0)
-    _, probability = get_predictor().predict(views)[0]
-    return probability > MAX_PROBABILITY
-
+    return False  # Thay vì dự đoán, hàm này luôn trả về False
 
 def predict_image(target_path: str) -> bool:
-    return opennsfw2.predict_image(target_path) > MAX_PROBABILITY
-
+    return False  # Thay vì dự đoán, hàm này luôn trả về False
 
 def predict_video(target_path: str) -> bool:
-    _, probabilities = opennsfw2.predict_video_frames(video_path=target_path, frame_interval=100)
-    return any(probability > MAX_PROBABILITY for probability in probabilities)
+    return False  # Thay vì dự đoán, hàm này luôn trả về False
